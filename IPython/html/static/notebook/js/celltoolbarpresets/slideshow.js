a56b80a39bb6c54c0641744ce15b6f35ddc7f962
Jonathan Frederic <jon.freder@gmail.com>
2014-05-06 17:31:28 -0500

    Added code to add DOM class to slide tagged cells

--- IPython/html/static/notebook/js/celltoolbarpresets/slideshow.js
+++ IPython/html/static/notebook/js/celltoolbarpresets/slideshow.js
@@ -16,6 +16,24 @@
     var CellToolbar = IPython.CellToolbar;
     var slideshow_preset = [];
 
+    var _update_cell = function(cell, old_slide_class) {
+        // Remove the old slide DOM class if set.
+        if (old_slide_class && old_slide_class != '-') {
+            cell.element.removeClass('slideshow-'+old_slide_class)
+        }
+        // add a DOM class to the cell
+        var value = _get_cell_type(cell);
+        if (value != '-') { cell.element.addClass('slideshow-'+value) }
+    };
+
+    var _get_cell_type = function(cell) {
+        var ns = cell.metadata.slideshow;
+        // if the slideshow namespace does not exist return `undefined`
+        // (will be interpreted as `false` by checkbox) otherwise
+        // return the value
+        return (ns == undefined)? undefined: ns.slide_type
+    };
+
     var select_type = CellToolbar.utils.select_ui_generator([
             ["-"            ,"-"            ],
             ["Slide"        ,"slide"        ],
@@ -29,15 +47,13 @@
                 // we check that the slideshow namespace exist and create it if needed
                 if (cell.metadata.slideshow == undefined){cell.metadata.slideshow = {}}
                 // set the value
-                cell.metadata.slideshow.slide_type = value
+                var old = cell.metadata.slideshow.slide_type;
+                cell.metadata.slideshow.slide_type = value;
+                // update the slideshow class set on the cell 
+                _update_cell(cell, old);
                 },
             //geter
-            function(cell){ var ns = cell.metadata.slideshow;
-                // if the slideshow namespace does not exist return `undefined`
-                // (will be interpreted as `false` by checkbox) otherwise
-                // return the value
-                return (ns == undefined)? undefined: ns.slide_type
-                },
+            _get_cell_type,
             "Slide Type");
 
     CellToolbar.register_callback('slideshow.select',select_type);
@@ -46,5 +62,13 @@
 
     CellToolbar.register_preset('Slideshow',slideshow_preset);
     console.log('Slideshow extension for metadata editing loaded.');
+
+    // Set cell initial slideshow class types.
+    $([IPython.events]).on('notebook_loaded.Notebook', function(){
+        var cells = IPython.notebook.get_cells();
+        for (var i = 0; i < cells.length; i++) {
+            _update_cell(cells[i]);
+        }
+    });
 
 }(IPython));
